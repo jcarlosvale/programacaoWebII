c9b2ef01e42f2449ea7335891554de1cfdc628cf
Joao Vale <jcarlos.vale@gmail.com>
2023-01-22 20:40:39 +0000

    initial commit

---
+++ src/main/java/com/study/SchoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class SchoolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/SchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class SchoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
