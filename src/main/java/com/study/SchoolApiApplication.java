e702211a5c9e99a2bb1bfbb7084e7fe50be7140e
Joao Vale <jcarlos.vale@gmail.com>
2023-01-27 22:40:49 +0000

    hello world

--- src/main/java/com/study/SchoolApiApplication.java
+++ src/main/java/com/study/SchoolApiApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-public class SchoolApiApplication {
+public class SchoolApiApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World");
+	}
 }
