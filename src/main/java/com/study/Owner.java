98e74522b45f02a8efe4ff13cda07942ad6adf40
Joao Vale <jcarlos.vale@gmail.com>
2023-01-27 22:48:10 +0000

    simple commit

---
+++ src/main/java/com/study/Owner.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Owner {
+
+    private String name;
+
+}
--- src/main/java/com/study/SchoolApiApplication.java
+++ src/main/java/com/study/SchoolApiApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.*;
 @SpringBootApplication
 public class SchoolApiApplication implements CommandLineRunner{
 
+
+	private Owner owner;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApiApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+
 	}
 }
