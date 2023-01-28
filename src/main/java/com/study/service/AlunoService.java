4198b7d7aa465c966f9da50dc3d2f28095eefe9c
Rogerio Colaco da Silva <colaco.rogerio@gmail.com>
2023-01-27 21:21:47 -0300

    Ciacao: classes

---
+++ src/main/java/com/study/service/AlunoService.java
@@ -0,0 +1,4 @@
+package com.study.service;
+
+public class AlunoService {
+}
---
+++ src/main/java/com/study/view/rs/v2/AlunoControllerV2.java
@@ -0,0 +1,11 @@
+package com.study.view.rs.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v2/alunos")
+@Slf4j
+public class AlunoControllerV2 {
+}
