401bdc5c72dc146797000638f6dc635b9f8491d7
SAMILA MILENA DIAS DE PAULA <60451467+Samila23@users.noreply.github.com>
2023-01-27 21:20:00 -0300

    Add files via upload

---
+++ programacaoWebII/src/main/java/com/study/domain/dto/AlunoDto.java
@@ -0,0 +1,18 @@
+package com.study.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties
+public class AlunoDto {
+    private int id;
+
+    private String nome;
+}
