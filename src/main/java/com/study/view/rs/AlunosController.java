57618611ce2aa39705964a91d75ea590207f9044
MGLMendes <miguelmendes203@gmail.com>
2023-01-27 21:43:15 -0300

    Atualizando Aluno Service

--- src/main/java/com/study/view/rs/AlunosController.java
+++ src/main/java/com/study/view/rs/AlunosController.java
@@ -1,8 +1,10 @@
 package com.study.view.rs;
 
 import com.study.domain.dto.*;
+import com.study.domain.service.AlunoService;
 import lombok.*;
 import lombok.extern.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,36 +17,27 @@ import java.util.stream.*;
 @Slf4j
 public class AlunosController {
 
-    private final Map<Integer, AlunosDto> repository;
+    private final AlunoService service;
+
 
     //@GetMapping  comentado para evitar conflito de endpoint com request param abaixo
     public ResponseEntity<List<AlunosDto>> getAll() {
         log.info("Listing alunos");
-        if (repository.isEmpty()) {
+
+        List<AlunosDto> listaDeAlunos = service.retrieveAll();
+        if (listaDeAlunos.isEmpty()) {
             return ResponseEntity.ok(List.of());
         }
         else {
             return ResponseEntity
-                    .ok(new ArrayList<>(repository.values()));
+                    .ok(new ArrayList<>(listaDeAlunos));
         }
-    }
-
-    @GetMapping
-    public ResponseEntity<List<AlunosDto>> getByPrefix(@RequestParam(value = "prefixo", required = false) final String prefixo) {
-        log.info("Getting cor with prefix {}", prefixo);
-        if(Objects.isNull(prefixo)) return getAll();
-        var selectedCores =
-                repository.values().stream()
-                .filter(aluno -> aluno.getNome().startsWith(prefixo))
-                .collect(Collectors.toList());
-        return ResponseEntity
-                .ok(selectedCores);
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<AlunosDto> getById(@PathVariable("id") final int id) {
         log.info("Getting aluno {}", id);
-        var aluno = repository.get(id);
+        var aluno = service.getById(id);
         if (Objects.isNull(aluno)) {
             return ResponseEntity
                     .notFound()
@@ -58,14 +51,14 @@ public class AlunosController {
 
     @PostMapping
     public ResponseEntity<AlunosDto> save(@RequestBody final AlunosDto aluno) {
-        if (repository.containsKey(aluno.getId())) {
+        if (service.getById(aluno.getId()) != null) {
             log.error("Collection contains id {}", aluno.getId());
             return ResponseEntity
                     .status(HttpStatus.CONFLICT)
                     .build();
         }
         else {
-            repository.put(aluno.getId(), aluno);
+            service.save(aluno);
             log.info("Inserted a new aluno {}", aluno);
             return ResponseEntity
                     .status(HttpStatus.CREATED)
@@ -76,29 +69,31 @@ public class AlunosController {
     @PutMapping(path = "/{id}")
     public ResponseEntity<AlunosDto> update(@PathVariable("id") final int id, @RequestBody final AlunosDto alunosDto) {
         log.info("Updating aluno {}", id);
-        if (!repository.containsKey(alunosDto.getId())) {
-            return ResponseEntity
-                    .notFound()
-                    .build();
+        AlunosDto aluno = service.getById(id);
+
+        if (aluno == null) {
+            return ResponseEntity.notFound().build();
         }
-        else {
-            repository.put(id, alunosDto);
-            return ResponseEntity
+
+        service.update(id, alunosDto);
+
+        return ResponseEntity
                     .ok(alunosDto);
-        }
+
     }
 
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") final int id) {
         log.info("Deleting aluno {}", id);
-        var aluno = repository.get(id);
+        var aluno = service.getById(id);
+
         if (Objects.isNull(aluno)) {
             return ResponseEntity
                     .notFound()
                     .build();
         }
         else {
-            repository.remove(id);
+            service.delete(id);
             return ResponseEntity
                     .noContent()
                     .build();
