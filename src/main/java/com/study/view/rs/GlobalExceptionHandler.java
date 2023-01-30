0a2527da34a68af9752ffd54b141a994000b3c52
MGLMendes <miguelmendes203@gmail.com>
2023-01-30 20:12:20 -0300

    Criado classe para lidar com excessões

---
+++ src/main/java/com/study/view/rs/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.study.view.rs;
+
+import com.study.domain.dto.ErrorResponse;
+import jdk.dynalink.linker.MethodHandleTransformer;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Log4j2
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintVialotionException(
+            final MethodArgumentNotValidException methodArgumentNotValidException) {
+
+        return ResponseEntity.badRequest().body(ErrorResponse.createFormValidation(methodArgumentNotValidException));
+    }
+}
