cfeb4bb4e0fea327b2c3f1637bc34d08e4d6dc46
Michelle <michelledantasluiz@gmail.com>
2023-02-01 20:29:58 -0300

    Correção do Aluno getByID

--- src/main/java/com/study/mappers/AlunoMapper.java
+++ src/main/java/com/study/mappers/AlunoMapper.java
@@ -1,20 +1,18 @@
 package com.study.mappers;
 
-import com.study.domain.dto.AlunoDTO;
-import com.study.domain.dto.request.AlunoRequestDTO;
+
 import com.study.domain.dto.response.AlunoResponseDTO;
 import com.study.entity.Aluno;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
 public class AlunoMapper {
 
-    public AlunoResponseDTO toResponseDTO(Optional<Aluno> byId) { //TODO CORRIGIR
-        if (byId == null){
+    public AlunoResponseDTO toResponseDTO (Aluno aluno){
+        if (aluno == null){
             return null;
         }
 
@@ -43,7 +41,4 @@ public class AlunoMapper {
         return listaAlunos.stream().map(this::toResponseDTO).collect(Collectors.toList());
     }
 
-    public AlunoResponseDTO toResponseDTO(Optional<Aluno> byId) {
-
-    }
 }
--- src/main/java/com/study/service/AlunoService.java
+++ src/main/java/com/study/service/AlunoService.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import com.study.domain.dto.response.AlunoResponseDTO;
 import com.study.mappers.AlunoMapper;
 import com.study.repository.AlunoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.study.domain.dto.AlunoDTO;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class AlunoService {
 
 	private final AlunoRepository repository;
@@ -25,7 +27,14 @@ public class AlunoService {
 	}
 	
 	public AlunoResponseDTO getById(final int id) {
-		return mapper.toResponseDTO(repository.findById(id));
+		log.info("Retornando aluno por id{}", id);
+
+		var optionalAluno = repository.findById(id);
+
+		if(optionalAluno.isPresent()){
+			return mapper.toResponseDTO(optionalAluno.get());
+		}
+		return new AlunoResponseDTO();
 	}
 	
 	public AlunoDTO save(final AlunoDTO aluno) {
@@ -34,13 +43,13 @@ public class AlunoService {
 	}
 	
 	public AlunoDTO update(final int id, final AlunoDTO aluno) {
-		repository.put(id, aluno);
+		repository.save(id, aluno);
 		return aluno;
 	}
 	
 	public void delete(final int id) throws Exception {
-		if(repository.containsKey(id)) {
-			repository.remove(id);
+		if(repository.findById(id)) {
+			repository.delete(id);
 		} else {
 			throw new Exception("Id não encontrado");
 		}
