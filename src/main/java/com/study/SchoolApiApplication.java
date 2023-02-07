d81c81a7b2f42bf923487652d88c8ab55d354f6e
MGLMendes <miguelmendes203@gmail.com>
2023-02-06 21:12:39 -0300

    Populando banco de dados com cargas iniciais

--- src/main/java/com/study/SchoolApiApplication.java
+++ src/main/java/com/study/SchoolApiApplication.java
@@ -1,12 +1,25 @@
 package com.study;
 
+import com.study.domain.dto.DisciplinasResponse;
+import com.study.domain.model.Alunos;
 import com.study.domain.model.Disciplinas;
 import com.study.domain.model.Professores;
+import com.study.domain.repositories.AlunosRepository;
+import com.study.domain.repositories.DisciplinaRepository;
+import com.study.domain.repositories.ProfessoresRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class SchoolApiApplication implements CommandLineRunner {
+
+
+	private final ProfessoresRepository professoresRepository;
+	private final AlunosRepository alunosRepository;
+	private final DisciplinaRepository disciplinaRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApiApplication.class, args);
@@ -14,6 +27,53 @@ public class SchoolApiApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		var professor1 =
+				Professores.builder()
+						.nome("Jose")
+						.titulo("Professor de Spring")
+						.sexo("Masculino")
+						.build();
+		var professor2 =
+				Professores.builder()
+						.nome("Maria")
+						.titulo("Professora de Artes")
+						.sexo("Masculino")
+						.build();
+
+		professoresRepository.save(professor1);
+		professoresRepository.save(professor2);
+
+
+		var aluno1 =
+				Alunos.builder()
+						.nome("AlunoX")
+						.build();
+		var aluno2 =
+				Alunos.builder()
+						.nome("AlunoY")
+						.build();
+
+		alunosRepository.save(aluno1);
+		alunosRepository.save(aluno2);
+
+		var disciplina1 =
+				Disciplinas.builder()
+						.nome("Spring Boot")
+						.descricao("Materia sobre Spring Boot")
+						.duracao("80h")
+						.titular(professor1)
+						.build();
+
+		var disciplina2 =
+				Disciplinas.builder()
+						.nome("Artes")
+						.descricao("Materia sobre Artes")
+						.duracao("80h")
+						.titular(professor2)
+						.build();
+
+		disciplinaRepository.save(disciplina1);
+		disciplinaRepository.save(disciplina2);
 
 	}
 }
--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO ALUNOS (aluno_name, matricula, sexo) VALUES ('Marcio', '123', 'M');
-INSERT INTO ALUNOS (aluno_name, matricula, sexo) VALUES ('Miguel', '321', 'M');
-
-
-INSERT INTO DISCIPLINAS  (disciplina_name, descricao, duracao, disciplinas) values ('Spring Boot', 'Materia Spring Boot', '80h', 1)
-
-INSERT INTO PROFESSORES (professor_name, titulo, sexo, titular_id) VALUES ('Professor', 'Professor de Spring', 'M', 1);
