ade26014125e884cb610c7b7e7ff1b075d06439b
Letícia Viana <97035393+Leticia-Viana@users.noreply.github.com>
2023-02-08 22:14:23 -0300

    Create ToDoDto.java
    Add class ToDoDto

---
+++ src/main/java/com/study/dto/ToDoDto.java
@@ -0,0 +1,20 @@
+package com.study.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties
+public class ToDoDto {
+
+    @JsonProperty(value="activity")
+    private String description;
+
+}
