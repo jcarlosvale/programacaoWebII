aef8f93022751dbfae50f0e9dec9262e4c206a4e
jeronimo mariano <jeronimo.mariano@f1rst.com.br>
2023-02-08 21:14:46 -0300

    etapa de autorização

--- src/main/java/com/study/configuration/SecurityConfiguration.java
+++ src/main/java/com/study/configuration/SecurityConfiguration.java
@@ -4,17 +4,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 	@Autowired
+	PasswordEncoder passwordEncoder;
+	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().
+		auth.inMemoryAuthentication()
+        .withUser("professor")
+        .password(passwordEncoder.encode("1234"))
+        .authorities("ROLE_PROFESSOR")
+        .and()
+        .withUser("aluno")
+        .password(passwordEncoder.encode("1234"))
+        .authorities("ROLE_ALUNO");
 	}
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+        .authorizeRequests()
+        .antMatchers("/h2-console/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+return http.build();
 
 	}
 }
