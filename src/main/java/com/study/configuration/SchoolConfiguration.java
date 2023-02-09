a756e0abe8caced21d3f899b61b3f0e8d67f5661
Letícia Viana <97035393+Leticia-Viana@users.noreply.github.com>
2023-02-08 22:12:09 -0300

    Update SchoolConfiguration.java
    Add RestTemplate

--- src/main/java/com/study/configuration/SchoolConfiguration.java
+++ src/main/java/com/study/configuration/SchoolConfiguration.java
@@ -3,6 +3,7 @@ package com.study.configuration;
 import com.study.Employee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class SchoolConfiguration {
@@ -10,6 +11,11 @@ public class SchoolConfiguration {
     @Bean
     public Employee employee() {
         return new Employee("Jose");
+    }
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
